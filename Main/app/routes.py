48d50998ed1ecf0e8027faf200d79d56c576ed27
Joseph <japoncen@uce.edu.ec>
2025-01-19 16:48:45 -0500

    Control de errores con if(routes.py)  y Try-exc.(recipe_service)

--- Main/app/routes.py
+++ Main/app/routes.py
@@ -1,8 +1,28 @@
 from flask import Flask, request, jsonify
+from app.services.recipe_service import recipeService
+
 
 def init_routes(app):
     @app.route('/ingredients', methods=['POST'])
+    
     def get_recipes():
+        #Obtener los ingredientes
         data = request.json
+        ingredients = data.get("ingredients")
         
-        return jsonify({"message": "Recetas personalizadas generadas"})
+        if not ingredients:
+            return jsonify({"error": "No se ingresó "}), 400   
+        
+        #Instancia y obtención de recetas
+        recipe_service = recipeService()
+        recipe = recipe_service.fetch_recipes(ingredients)
+        
+        if "error" in recipes:
+            return jsonify({"error": recipes["error"]}), 500
+        
+        return jsonify({"Recetas": recipes]})
+       
+        
+        #return jsonify({"message": "Recetas personalizadas generadas"})
+        
+        
--- Main/app/services/recipe_service.py
+++ Main/app/services/recipe_service.py
@@ -1,15 +1,19 @@
 import requests
 import os
-# Cargar las variables de entorno desde el archivo .env
 
-class RecipeService:
+
+class recipeService:
     def __init__(self):
         # Cargar las variables de entorno directamente
         self.api_key = os.getenv('GEMINI_API_KEY')
         self.url = os.getenv('GEMINI_API_URL')
 
     def fetch_recipes(self, ingredients):
-        response = requests.post(self.url, 
+        try:
+            response = requests.post(self.url, 
                                  json={"ingredients": ingredients},
                                  headers={"Authorization": f"Bearer {self.api_key}"})
-        return response.json()
+            response.raise_forstatus()
+            return response.json()
+        except requests.exceptions.RequestException as rqst:
+            return {"error":str(rqst)}    
