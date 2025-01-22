ab5846c183020088433fd1f6d2eb044c053daff6
Jccasav14 <jeancasaxd60@gmail.com>
2025-01-22 16:11:40 -0500

    Agregar en .env comando para conectar a bd, agregar a __init__.oy metodo de cnexion a bd, actualizar requeriments

--- Main/app/__init__.py
+++ Main/app/__init__.py
@@ -2,8 +2,12 @@ from flask import Flask
 from app.routes import init_routes
 from dotenv import load_dotenv 
 from app.routes import init_routes
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 import os
 
+db = SQLAlchemy()
+migrate = Migrate()
 
 def create_app():
     
@@ -13,4 +17,22 @@ def create_app():
     with app.app_context():
         init_routes(app)
         
+    return app
+
+
+def create_app():
+    # Carga las variables de entorno desde el archivo .env
+    load_dotenv()
+
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    # Registrar blueprints si es necesario
+    from app.routes import bp as main_bp
+    app.register_blueprint(main_bp)
+
     return app
